24b6e7a07d028163beb7f54bf36adc3a4cf97bae
Matthew Mayer <matthewkmayer@gmail.com>
2015-10-20 16:48:46 -0700

    Bare bones java console app.

---
+++ src/main/java/com/datapipe/javasqsworker/Main.java
@@ -0,0 +1,13 @@
+package com.datapipe.javasqsworker;
+
+public class Main {
+    public static void main(String[] args) {
+        // Read in args
+
+        // "work" on the args
+
+
+        // TODO: write out local filesystem location of output
+        System.out.println("Done, output location is: ");
+    }
+}
