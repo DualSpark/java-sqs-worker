f00da544805b51808b21114f2d58883fbca43f66
Matthew Mayer <matthewkmayer@gmail.com>
2015-10-21 09:08:44 -0700

    Updates server python script.

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import json
 # Get the service resource
 sqs_service = boto3.resource('sqs', region_name='us-east-1')
 sqs_client = boto3.client('sqs', region_name='us-east-1')
+s3_client = boto3.client('s3', region_name='us-east-1')
 
 # Get the queue. This returns an SQS.Queue instance
 queue = sqs_service.get_queue_by_name(QueueName='javasqsworker')
@@ -18,11 +19,13 @@ body = json.loads(message.body)
 #sqs_client.delete_message(QueueUrl=queue.url,ReceiptHandle=message.receipt_handle)
 
 # get where to download things from S3
-print("constraints_location from body: ", body['constraints_location'])
+constraints_location = body['constraints_location']
+print("constraints_location from body: ", constraints_location)
 
 # download from S3
+s3.download_file("bucketname", "filename", "localfilename")
 
 # run program
 
 # upload results to S3
-
+#s3.Bucket('mybucket').upload_file('/tmp/hello.txt', 'hello.txt')
