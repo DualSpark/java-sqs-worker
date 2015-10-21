25db2f643d865c88df17004b618d7cc33bc47d07
Matthew Mayer <matthewkmayer@gmail.com>
2015-10-21 14:36:03 -0700

    Greatly improves client and server scripts.

--- scripts/client.sh
+++ scripts/client.sh
@@ -16,31 +16,17 @@ timestamp="$(date +"%s")"
 zip job-input.zip $1 $2
 
 # upload job-input.zip to S3 under the timestamp dir:
-echo "Would send job-input.zip to s3://javasqsworker12345/$timestamp/job-input.zip"
-#aws s3 cp job-input.zip s3://javasqsworker12345/$timestamp/job-input.zip
+# echo "Would send job-input.zip to s3://javasqsworker12345/$timestamp/job-input.zip"
+aws s3 cp job-input.zip s3://javasqsworker12345/$timestamp/job-input.zip
 
 # Send work message to SQS: needs bucket name, folder name to use.
 # Server will assume job-input.zip is in bucket/foldername.
-echo '{"bucket":"javasqsworker12345", "folder":"'$timestamp'"}'
 
+# echo 'Would send this JSON in SQS message: {"bucket":"javasqsworker12345", "folder":"'$timestamp'"}'
+echo '{"bucket":"javasqsworker12345", "folder":"'$timestamp'"}' > sqs-message.json
+
+aws sqs send-message --region us-east-1 --queue-url https://sqs.us-east-1.amazonaws.com/347452556413/javasqsworker \
+--message-body file://sqs-message.json
 
 # clean up:
-rm job-input.zip
-
-
-
-# upload constraints to S3
-#echo "Uploading parameters file $2 to S3..."
-#aws s3 cp $2 s3://javasqsworker12345/
-
-# sed inline job definition: replace CONSTRAINTSLOCATION with the location we got back from S3 above
-#cat $1 | sed -e "s/CONSTRAINTSLOCATION/s3:\/\/javasqsworker12345\/$2/" > ready-job.json
-
-# Send message to SQS: https://sqs.us-east-1.amazonaws.com/347452556413/javasqsworker
-#echo "Sending job description based off $1 to SQS..."
-#aws sqs send-message --region us-east-1 --queue-url https://sqs.us-east-1.amazonaws.com/347452556413/javasqsworker \
-#--message-body file://ready-job.json
-
-# clean up our temp file
-#echo "Cleaning up..."
-#rm ready-job.json
+rm job-input.zip sqs-message.json
--- scripts/server.py
+++ scripts/server.py
@@ -10,31 +10,60 @@ s3_client = boto3.client('s3', region_name='us-east-1')
 # Get the queue. This returns an SQS.Queue instance
 queue = sqs_service.get_queue_by_name(QueueName='javasqsworker')
 
+# TODO: loop around the remainder of the file, forever!
+
 # Grab the first messsage
 message_list = queue.receive_messages(MaxNumberOfMessages=1, WaitTimeSeconds=5)
-message = message_list[0]
-body = json.loads(message.body)
-#print("First message body: ", body)
 
-# Save message body (job description) to disk:
-# Save to job.json
+# TODO: don't assume we received a message
+if len(message_list) == 0:
+    print("Didn't get a message, waiting and trying again...")
+    exit(1) # TODO: continue in infinite loop
+
+message = message_list[0]
 
 # delete message
-# disabled for debug/dev purposes
-#sqs_client.delete_message(QueueUrl=queue.url,ReceiptHandle=message.receipt_handle)
+sqs_client.delete_message(QueueUrl=queue.url,ReceiptHandle=message.receipt_handle)
 
-# get where to download things from S3
-constraints_location = body['constraints_location']
-print("constraints_location from body: ", constraints_location)
+body = json.loads(message.body)
 
-# download from S3
-# Temp hardcoding, later on we'll extract from constraints_location
-s3_client.download_file('javasqsworker12345', 'sample-constraints-1.json', 'constraints.json')
+# body has two fields: bucket and folder.  Extract those.
+bucket = body['bucket']
+folder = body['folder']
 
-# run program
-call(["java", "-jar javasqsworker-1.0-SNAPSHOT.jar job.json constraints.json"])
+call(["mkdir", "temp-work"])
 
-# upload results to S3
-#s3_client.Bucket('javasqsworker12345').upload_file('output.zip', 'output.zip')
+object_location = folder + '/job-input.zip'
+print('downloading object at ' + object_location)
+
+# download job-input.zip:
+s3_client.download_file(bucket, object_location, 'temp-work/job-input.zip')
+
+# unzip job-input
+call(["unzip", "temp-work/job-input.zip", "-d", "temp-work/"])
+
+# run bidmaster on job-input
+print("Would have called the java process here")
+# pretending we have results from bidmaster:
+f = open('temp-work/logfile.log', 'w')
+f.write('logs')
+f.close()
+f = open('temp-work/stdout.log', 'w')
+f.write('stdout')
+f.close()
+f = open('temp-work/results.txt', 'w')
+f.write('results')
+f.close()
+
+
+# call(["java", "-jar javasqsworker-1.0-SNAPSHOT.jar temp-work/job.json temp-work/constraints.json"])
+
+# zip results, log file, stdout from bidmaster to job-output.zip
+call(["zip", "temp-work/job-output.zip", "temp-work/logfile.log", "temp-work/stdout.log", "temp-work/results.txt"])
+
+# upload results to bucket/folder/job-output.zip
+s3_client.upload_file('temp-work/job-output.zip', bucket, folder + '/job-output.zip')
 
 # Clean up job, constraints and output files.
+# print("Would have deleted the temp-work folder here")
+call(["rm", "-rf", "temp-work/"])
