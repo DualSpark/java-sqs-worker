15f35ad7a2d734d8adef53498cd085fa3948dfa6
Matthew Mayer <matthewkmayer@gmail.com>
2015-10-27 18:39:13 -0700

    Shows java pid checking works.

--- scripts/temp-alarm-checker.py
+++ scripts/temp-alarm-checker.py
@@ -1,5 +1,6 @@
 import boto3
 import logging
+import psutil
 
 logging.basicConfig(format='%(asctime)s %(message)s')
 
@@ -14,3 +15,13 @@ if len(alarm['MetricAlarms']) > 0:
     logging.warning('we got something in metricalarms')
     if alarm['MetricAlarms'][0]['StateValue'] == 'ALARM':
         logging.warning('in alarm state, we should bail')
+
+# now for java checking:
+logging.warning('checking pids')
+for p in psutil.process_iter():
+    if p.name() == 'java':
+        logging.warning('java is running!')
+        exit(0)
+
+logging.warning('java not running')
+
