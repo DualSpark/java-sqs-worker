4e318323a877c967796b69dced0710936c5aa60d
Matthew Mayer <matthewkmayer@gmail.com>
2015-10-27 18:47:46 -0700

    Finishes scaledown python process.

--- scripts/scaledown.py
+++ scripts/scaledown.py
@@ -1,11 +1,19 @@
 import boto3
 import logging
+import psutil
+import time
 from subprocess import check_output
 
 
 def java_is_running():
-    # We'll need to actually check here, of course
-    return True
+    logging.warning('checking if java is running')
+    for p in psutil.process_iter():
+        if p.name() == 'java':
+            logging.warning('java is running')
+            return True
+
+    logging.warning('java not running')
+    return False
 
 
 def queue_empty_alarm_tripped():
@@ -32,18 +40,20 @@ def main():
     if queue_empty_alarm_tripped():
         # We're not running a job right now and the queue has been empty long enough to trip alarm.
         # Terminate ourselves.
-        instance_id = check_output(["ec2-metadata", "-i"]).replace('instance-id: ', '')
 
+        logging.warning('Java not running on this instance and queue empty alarm tripped: terminating this instance.')
+
+        instance_id = check_output(["ec2-metadata", "-i"]).replace('instance-id: ', '')
         logging.warning('We are instance id %s', instance_id)
 
         client = boto3.client('autoscaling')
-
-        response = client.terminate_instance_in_auto_scaling_group(
+        client.terminate_instance_in_auto_scaling_group(
             InstanceId=instance_id,
             ShouldDecrementDesiredCapacity=True
         )
 
-        # and we should be shutting down soon.
+        # and we should be shutting down soon.  Stay here until we do:
+        time.sleep(10000)
 
 
 if __name__ == "__main__":
